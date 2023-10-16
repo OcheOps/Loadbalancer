2cc89ebcedf7aff1d1fb10dcb1027e41f4357ebf
OcheOps <ochecodes@gmail.com>
2023-10-16 20:35:12 +0100

    Func handle err

--- src/main.go
+++ src/main.go
@@ -1,9 +1,20 @@
 package main
 
 import(
-"net/http/http.util"
-"net/url"
+	"fmt"
+	"net/http/http.util"
+	"net/url"
+	"os"
+
 )
+
+
+type Server struct {
+
+	Address() string
+	IsAlive() bool
+	Serve(rw http.ResponseWriter, req *http.Request)
+}
 
 type SimpleServer struct {
 
@@ -15,9 +26,12 @@ type SimpleServer struct {
 
 func NewSimpleServer(addr string) *SimpleServer {
 	ServerUrl, err := url.Parse(addr)
-	if err != nil {
-		panic(err)
-	}
+	//if err != nil {
+	//	panic(err)
+	//}
+
+	handleErr(err)
+
 	return &SimpleServer{
 		addr: addr,
 		proxy: http.util.NewSingleHostReverseProxy(ServerUrl),
@@ -29,3 +43,17 @@ func NewSimpleServer(addr string) *SimpleServer {
 //func (s *SimpleServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 //	s.proxy.ServeHTTP(w, r)
 //}
+ type LoadBalance struct {
+	port string
+	roundRobinCount int
+	servers []Server
+ }
+
+
+func handleErr(err error){
+	if err !=nil{
+		fmt.Print("error :%v/n",err)
+		os.Exit(1)
+	
+	}
+}
