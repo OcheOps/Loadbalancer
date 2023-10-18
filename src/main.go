047b38c083327a1d2bd22bbe56a9fb7712345869
OcheOps <ochecodes@gmail.com>
2023-10-18 01:15:43 +0100

    Almost done

--- src/main.go
+++ src/main.go
@@ -5,11 +5,12 @@ import(
 	"net/http/http.util"
 	"net/url"
 	"os"
+	"net/http"
 
 )
 
 
-type Server struct {
+type Server interface {
 
 	Address() string
 	IsAlive() bool
@@ -43,17 +44,74 @@ func NewSimpleServer(addr string) *SimpleServer {
 //func (s *SimpleServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 //	s.proxy.ServeHTTP(w, r)
 //}
- type LoadBalance struct {
+ type Loadbalancer struct {
 	port string
 	roundRobinCount int
 	servers []Server
  }
 
+ 
 
+
+func NewLoadbalancer(port string, servers[]Server ) *Loadbalancer{
+	return &Loadbalancer{
+		port: port,
+		roundRobinCount: 0,
+		servers: make([]Server,0),
+	}
+}
+
+//}
 func handleErr(err error){
 	if err !=nil{
 		fmt.Print("error :%v/n",err)
 		os.Exit(1)
 	
 	}
+}
+
+ func (lb *Loadbalancer)  	getNextAvailableServer() Server{
+ 	nextAvailableServer := lb.servers[lb.roundRobinCount]
+ 	lb.roundRobinCount = (lb.roundRobinCount + 1) % len(lb.servers)
+ 	return nextAvailableServer
+ }
+
+func (lb *Loadbalancer) serveProxy(rw http.ResponseWriter, req *http.Request){
+	nextAvailableServer := lb.getNextAvailableServer()
+	nextAvailableServer.ServeHTTP(rw, req)
+}
+
+
+func Address(s *SimpleServer) string{
+	return s.addr
+}
+
+func IsAlive(s *SimpleServer) bool{
+	_, err := http.Get(s.addr)
+	if err != nil{
+		return false
+	}
+}
+
+
+func Serve(s *SimpleServer, rw http.ResponseWriter, req *http.Request){
+	targetServer := lb.getNextAvailableServer()
+	targetServer.ServeHTTP(rw, req)
+	s.proxy.ServeHTTP(rw, req)
+}
+
+
+
+func main(){
+	servers:=[]Server{
+		NewSimpleServer("http://localhost:3000"),
+		NewSimpleServer("http://localhost:3001"),
+		NewSimpleServer("http://localhost:3002"),
+
+	}
+	lb := NewLoadbalancer(":8080", servers)
+	http.HandleFunc("/", lb.serveProxy)
+	http.ListenAndServe(":8080", nil)
+
+	fmt.Println("serving at localhost")
 }
